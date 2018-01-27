40e8241f24d302584847b2077f4de78595f4320b
AkiTsuKiS <naqmjohn2003@hotmail.com>
2018-01-27 08:21:14 +0800

    sync

---
+++ QScripts/MainProc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainProc : MonoBehaviour {
+
+	public GameObject ground;
+	public GameObject border;
+
+	void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
