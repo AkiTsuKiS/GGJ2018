6a9431ef0f590b3beb35dfb59a17478756445a77
AkiTsuKiS <naqmjohn2003@hotmail.com>
2018-01-27 17:17:41 +0800

    microphoneHandler

---
+++ QScripts/MicrophoneHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneHandler : MonoBehaviour
+{
+	//*****************************************************//
+	//** STATIC *******************************************//
+	//*****************************************************//
+	private static MicrophoneHandler _instance = null;
+
+	private static void init()
+	{
+		if (_instance == null) 
+			_instance = GameObject.Find("MicrophoneHandler").GetComponent<MicrophoneHandler>();
+	}
+
+	public static void StartRecord()
+	{
+		init();
+		_instance.startRec();
+	}
+
+	public static void ReplayRecord()
+	{
+
+	}
+
+	public static void GetVolume()
+	{
+
+	}
+	//*****************************************************//
+
+	private AudioSource _audioSource;
+
+	public void startRec()
+	{
+		if (Microphone.IsRecording(null))
+		{
+			return;
+		}
+		else
+		{
+			_audioSource.clip = Microphone.Start(null, true, 1500, 44100);
+		}
+	}
+
+	public void StopRec()
+	{
+		if (Microphone.IsRecording(null))
+		{
+			Microphone.End(null);
+		}
+	}
+
+	public void ReplayRec()
+	{
+		_audioSource.Play();
+	}
+
+	private void Start()
+	{
+		_audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	private void Update()
+	{
+	/*	if (Microphone.IsRecording(null))
+		{
+			_audioSource.clip
+		}
+		*/
+	}
+}
